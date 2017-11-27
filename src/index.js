2267f9a8e2c47636731f09ee397ebcb4db80efe8
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-11-26 19:00:50 -0800

    Implemente redux, add root reducer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 import {BrowserRouter} from 'react-router-dom';
 
-
 import App from './components/app';
+import reducers from './reducers';
 import './styles/index.css';
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
 ReactDOM.render((
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 	),document.querySelector('.app'));
