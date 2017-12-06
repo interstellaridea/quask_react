6e930875bdc9581845f7fc2ac97d6c0257c045a9
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-12-05 23:46:20 -0800

    Action Creator to post to Rails API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
+// leave this on line 4 for tom
 export const CREATE_QUESTION = 'CREATE_QUESTION';
+const POST_QUESTION_URL = 'https://quask-api.herokuapp.com/questions';
 
-
-const ROOT_URL = 'https://quask-api.herokuapp.com/questions';
-
-export function createQuestion(question) {
-	const request = axios.post(ROOT_URL,question)
-	return {
-		type: CREATE_QUESTION,
-		payload: request
-	}
+export function createQuestion(fixed, callback) {
+	const request = axios.post(POST_QUESTION_URL,fixed)
+    .then(()=>{ callback() });
+    
+  return { type: CREATE_QUESTION, payload: request.data }
 }
