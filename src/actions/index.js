5c9ad9aaa4a0394908f5646036ddc915b1ec2c40
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-12-26 23:02:17 -0800

    Create actions for submitting answer for question, flesh out action for websock incoming data

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,25 @@ import axios from 'axios';
 
 // leave this on line 4 for tom
 export const CREATE_QUESTION = 'CREATE_QUESTION';
-const POST_QUESTION_URL = 'https://quask-api.herokuapp.com/questions';
+export const RECEIVED_QUESTION = 'RECEIVED_QUESTION';
+export const ANSWER_SUBMITTED = 'ANSWER_SUBMITTED';
 
-export function createQuestion(fixed, callback) {
-	const request = axios.post(POST_QUESTION_URL,fixed)
-    .then(()=>{ callback() });
-    
-  return { type: CREATE_QUESTION, payload: request.data }
+const POST_QUESTION_URL = 'https://quask-api.herokuapp.com/questions';
+const SUBMIT_ANSWER_URL = 'https://quask-api.herokuapp.com/responses';
+
+export function createQuestion(question_hash, callback) {
+	const request = axios.post( POST_QUESTION_URL, question_hash )
+    // the callback is is to redirect user to new websocket see components/ask.js
+    .then( () => callback() );
+    return { type: CREATE_QUESTION, payload: request.data };
 }
+
+// export function receivedQuestion(received_hash) {
+//   return { type: RECEIVED_QUESTION, payload: received_hash };
+// }
+
+export function submitAnswer(answer_hash, callback) {
+  const request = axios.post(SUBMIT_ANSWER_URL, answer_hash )
+        .then( () => callback() );
+    return { type: ANSWER_SUBMITTED, payload: request.data };
+} 
