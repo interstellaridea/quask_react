c543ce36c62c69317a7c6c61fafea8f43b904d20
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-12-26 23:06:01 -0800

    Add ability to post clicked answer

--- src/connections/question_connection.js
+++ src/connections/question_connection.js
@@ -1,5 +1,10 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react';
 import ActionCable from 'actioncable'
+import { submitAnswer } from '../actions/index';
+
+// uncomment when ready to add to root reducer
+// import { connect } from 'react-redux';
+// import questionReducer from '../reducers/reducer_question';
 
 class QuestionConnection extends Component {
 
@@ -18,20 +23,28 @@ class QuestionConnection extends Component {
         console.log('ActionCable Connected!');
       },
       received: (data) => {
-        console.log(data);
+        
+        //uncomment for connecting to root reducer
+        // receivedQuestion(data);
+
         this.setState({
           question: data.question,
           answers: data.answers
         });
+
       },
       create: (chatContent) => {}
     })
   }
 
+  handleSubmit(answer){
+    submitAnswer( { response: { user_id: 1 , answer_id: answer.id } } );
+    // redirect callback () => this.context.history.push('/answer') 
+  }
 
   renderAnswers() {
     const answer_list = this.state.answers.map( answer =>
-      <li className='list-group-item list-group-item-action' key={answer.id}>
+      <li onClick={this.handleSubmit.bind(this, answer)} className='list-group-item list-group-item-action' key={answer.id}>
         {answer.name}
       </li>
     );
@@ -53,4 +66,15 @@ class QuestionConnection extends Component {
 
 }
 
+
+// un comment when connecting to root reducer
+// function mapStateToProps({ received_question }) {
+//   return { question: received_question.question, answers: received_question.answers };
+// }
+// export default connect(mapStateToProps, {submitAnswer, receivedQuestion})(QuestionConnection);
 export default QuestionConnection;
+
+
+
+
+
