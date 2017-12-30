521c0ffdf2117d292070e156db8de60c5a6a9cc7
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-12-29 23:26:20 -0800

    Remove Exact paths and Link component

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Ask from './ask';
 import Answer from './answer';
-import routes from '../routes';
 import Navbar from './navbar';
 
 
 const Main = () => (
-  <main  className='main'>
+  <div  className='main'>
     <Switch>
-      <Route exact path='/ask' component={Ask} />
-      <Route exact path='/answer' component={Answer} />
+      <Route path='/ask' component={Ask} />
+      <Route path='/answer' component={Answer} />
     </Switch>
-  </main>
+  </div>
 )
 
 
