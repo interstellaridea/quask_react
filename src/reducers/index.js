82e44f42bbda22d97579abc90f1f990fca86e2cf
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-12-29 23:23:22 -0800

    Add reducer for incoming questions and answers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { reducer as formReducer } from 'redux-form';
 import { combineReducers } from 'redux';
+import questionReducer from './reducer_question';
 
 const rootReducer = combineReducers({
-  form: formReducer
+  form: formReducer,
+  received_question: questionReducer
 });
 
 export default rootReducer;
--- src/reducers/reducer_question.js
+++ src/reducers/reducer_question.js
@@ -1,16 +1,19 @@
 import { CREATE_QUESTION, RECEIVED_QUESTION, ANSWER_SUBMITTED } from '../actions/index';
 
-const INITIAL_STATE = { all: [], question: 'Not on right now', answers: null };
+const INITIAL_STATE = { question: {name: 'Debug test', id: 150}, answers: [{id:1, name: 'test answer'}] };
 
 export default function (state = INITIAL_STATE, action) {
 	switch(action.type) {
+
 	case CREATE_QUESTION:
-    console.log(action.payload);
-		return {  all: action.payload.data };
+		return state;
+
   case RECEIVED_QUESTION:
     return { question: action.payload.question, answers: action.payload.answers };
+
   case ANSWER_SUBMITTED:
     return state;
+
 	default:
 		return state;
 	}
